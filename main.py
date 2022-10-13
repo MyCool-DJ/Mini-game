47f697325094fbf89f895eb3d838fca5e8228a53
mycooldj.eth <58099602+MyCool-DJ@users.noreply.github.com>
2022-10-13 04:51:42 +0000

    The counter variable is now working.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 #day17 of 100days-of-code via REplit
 from getpass import getpass as input
 
@@ -9,38 +10,38 @@ player2_score = 0
 
 while True:
 
-    player1 = input("Player 1, enter your first turn: ")
-    player2 = input("Player 2, enter your first turn: ")
-    print("OK, let's see!")
+  player1 = input("Player 1, enter your first turn: ")
+  player2 = input("Player 2, enter your first turn: ")
+  print("OK, let's see!")
 
-    if (player1 == "Rock" or player1 == "rock"
-            or player1 == "🪨") and (player2 == "Paper" or player2 == "paper"
-                                    or player2 == "📄"):
-        print("Player 2 wins!!")
-    elif (player1 == "Paper" or player1 == "paper"
-          or player1 == "📄") and (player2 == "Rock" or player2 == "rock"
-                                  or player2 == "🪨"):
-        print("Player 1 wins!!")
-					
-																		
-    elif (player1 == "Rock" or player1 == "rock" or player1 == "🪨") and (
-            player2 == "Scissors" or player2 == "scissors") or player2 == "✂️":
-        print("Player 1 wins!")
-        player1_score += 1
-    elif (player1 == "Scissors"
-          or player1 == "scissors") and (player2 == "Rock" or player2 == "rock"
-                                         or player2 == "🪨"):
-        print("Player 2 wins!")
-    elif (player1 == "Paper" or player1 == "paper" or player1
-          == "📄") and (player2 == "Scissors" or player2 == "scissors"):
-        print("Player 2 wins!")
-    elif (player1 == "Scissors" or player1 == "scissors"
-          or player1 == "✂️") and (player2 == "Paper" or player2 == "paper"
-                                   or player2 == "📄"):
-        print("Player 1 wins!")
+  if (player1 == "Rock" or player1 == "rock" or player1 == "🪨") and (player2 == "Paper" or player2 == "paper"):
+    print("Player 2 wins!!")
+    player2_score += 1
+	#elif (player1 == "Rock" or player1 == "rock") and (player2 == "Rock" or player2 == "rock")
+	  #print("draw")
 
-    if player1_score == 3 or player2_score == 3:
-        print("Thanks for  playing")
-        exit()
-    else:
-        continue
+  elif (player1 == "Paper" or player1 == "paper" or player1 == "📄") and (player2 == "Rock" or player2 == "rock" or player2 == "🪨"):
+    print("Player 1 wins!!")
+    player1_score += 1
+
+  elif (player1 == "Rock" or player1 == "rock" or player1 == "🪨") and (player2 == "Scissors" or player2 == "scissors") or player2 == "✂️":
+    print("Player 1 wins!")
+    player1_score += 1 
+
+  elif (player1 == "Scissors" or player1 == "scissors") and (player2 == "Rock" or player2 == "rock" or player2 == "🪨"):
+    print("Player 2 wins!")
+    player2_score += 1
+
+  elif (player1 == "Paper" or player1 == "paper" or player1 == "📄") and (player2 == "Scissors" or player2 == "scissors"):
+    print("Player 2 wins!")
+    player2_score += 1
+
+  elif (player1 == "Scissors" or player1 == "scissors" or player1 == "✂️") and (player2 == "Paper" or player2 == "paper" or player2 == "📄"):
+    print("Player 1 wins!")
+    player1_score += 1
+
+  if player1_score == 3 or player2_score == 3:
+    print("Thanks for playing")
+    exit()
+  else:
+    continue
